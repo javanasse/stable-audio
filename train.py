8213b30f0e4539e03caa09cef53393ea4bf0e26f
javanasse <julian.vanasse@gmail.com>
2024-07-12 12:08:00 -0400

    WIP: debugging training

--- train.py
+++ train.py
@@ -9,7 +9,9 @@ from stable_audio_tools.models.utils import load_ckpt_state_dict, remove_weight_
 from stable_audio_tools.training import create_training_wrapper_from_config, create_demo_callback_from_config
 from stable_audio_tools.training.utils import copy_state_dict
 
-os.environ["WANDB__SERVICE_WAIT"] = "300"
+from cog import Input, Path
+
+from util import unpack
 
 class ExceptionCallback(pl.Callback):
     def on_exception(self, trainer, module, err):
@@ -21,125 +23,96 @@ class ModelConfigEmbedderCallback(pl.Callback):
 
     def on_save_checkpoint(self, trainer, pl_module, checkpoint):
         checkpoint["model_config"] = self.model_config
+        
+DATA_PATH = os.path.join('src', 'dataset')
 
-name = "test-training"
-path_dataset_config = 'dataset.json'
-path_model_config = 'model_config.json'
-path_model_ckpt_path = 'model.ckpt'
-batch_size = 1
-num_workers = 1
-path_save_dir = "chkpts"
-checkpoint_every = 1000
-num_gpus = 2
-ckpt_path = None
+paths = [DATA_PATH]
+for path in paths:
+    if not os.path.exists(path):
+        os.makedirs(paths)
 
-#Get JSON config from args.model_config
-with open(path_model_config) as f:
-    model_config = json.load(f)
+def train(
+    data_url: Path = Input(description="HTTPS URL of a file containg training data"),
+    checkpoint: Path = Input(description="HTTPS URL of the model checkpoint."),
+    learning_rate: float = Input(description="learning rate, for learning!", default=1e-4, ge=0),
+    seed: int = Input(description="random seed to use for training", default=None)
+) -> str:
+    # get data from url
+    # unpack(data_url, DATA_PATH)
+    return "hehe"
 
-with open(path_dataset_config) as f:
-    dataset_config = json.load(f)
+# from getdata import repopulate_dataset
 
-train_dl = create_dataloader_from_config(
-    dataset_config, 
-    batch_size=batch_size, 
-    num_workers=num_workers,
-    sample_rate=model_config["sample_rate"],
-    sample_size=model_config["sample_size"],
-    audio_channels=model_config.get("audio_channels", 2),
-)
+# os.environ["WANDB__SERVICE_WAIT"] = "300"
 
-model = create_model_from_config(model_config)
+# def train():
+    
+#     print("downloading data:")
+#     repopulate_dataset("dataset")
+    
+#     name = "test-training"
+#     path_dataset_config = 'dataset.json'
+#     path_model_config = 'model_config.json'
+#     path_model_ckpt_path = 'model.ckpt'
+#     batch_size = 1
+#     num_workers = 1
+#     path_save_dir = "chkpts"
+#     checkpoint_every = 1000
+#     num_gpus = 2
+#     ckpt_path = None
 
-copy_state_dict(model, load_ckpt_state_dict(path_model_ckpt_path))
+#     #Get JSON config from args.model_config
+#     with open(path_model_config) as f:
+#         model_config = json.load(f)
 
-training_wrapper = create_training_wrapper_from_config(model_config, model)
+#     with open(path_dataset_config) as f:
+#         dataset_config = json.load(f)
 
-wandb_logger = pl.loggers.WandbLogger(project=name)
-wandb_logger.watch(training_wrapper)
+#     train_dl = create_dataloader_from_config(
+#         dataset_config, 
+#         batch_size=batch_size, 
+#         num_workers=num_workers,
+#         sample_rate=model_config["sample_rate"],
+#         sample_size=model_config["sample_size"],
+#         audio_channels=model_config.get("audio_channels", 2),
+#     )
 
-exc_callback = ExceptionCallback()
+#     model = create_model_from_config(model_config)
 
-if path_save_dir and isinstance(wandb_logger.experiment.id, str):
-    checkpoint_dir = os.path.join(path_save_dir, wandb_logger.experiment.project, wandb_logger.experiment.id, "checkpoints") 
-else:
-    checkpoint_dir = None
+#     copy_state_dict(model, load_ckpt_state_dict(path_model_ckpt_path))
 
-ckpt_callback = pl.callbacks.ModelCheckpoint(every_n_train_steps=checkpoint_every, dirpath=checkpoint_dir, save_top_k=-1)
-save_model_config_callback = ModelConfigEmbedderCallback(model_config)
+#     training_wrapper = create_training_wrapper_from_config(model_config, model)
 
-demo_callback = create_demo_callback_from_config(model_config, demo_dl=train_dl)
+#     wandb_logger = pl.loggers.WandbLogger(project=name)
+#     wandb_logger.watch(training_wrapper)
 
-strategy = 'ddp_find_unused_parameters_true' if num_gpus > 1 else "auto" 
+#     exc_callback = ExceptionCallback()
 
-trainer = pl.Trainer(
-        devices=num_gpus,
-        accelerator="gpu",
-        num_nodes = 1,
-        strategy=strategy,
-        precision="16-mixed",
-        accumulate_grad_batches=1, 
-        callbacks=[ckpt_callback, demo_callback, exc_callback, save_model_config_callback],
-        logger=wandb_logger,
-        log_every_n_steps=1,
-        max_epochs=10000000,
-        default_root_dir=path_save_dir,
-        gradient_clip_val=0.0,
-        reload_dataloaders_every_n_epochs = 0
-    )
-trainer.fit(training_wrapper, train_dl, ckpt_path=ckpt_path if ckpt_path else None)
+#     if path_save_dir and isinstance(wandb_logger.experiment.id, str):
+#         checkpoint_dir = os.path.join(path_save_dir, wandb_logger.experiment.project, wandb_logger.experiment.id, "checkpoints") 
+#     else:
+#         checkpoint_dir = None
 
-''''''
-# device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+#     ckpt_callback = pl.callbacks.ModelCheckpoint(every_n_train_steps=checkpoint_every, dirpath=checkpoint_dir, save_top_k=-1)
+#     save_model_config_callback = ModelConfigEmbedderCallback(model_config)
 
-# path_model_config = 'model_config.json'
-# path_model_ckpt_path = 'model.ckpt'
+#     demo_callback = create_demo_callback_from_config(model_config, demo_dl=train_dl)
 
-# with open(path_model_config) as f:
-#     model_config = json.load(f)
-# model = stable_audio_tools.create_model_from_config(model_config)
-# stable_audio_tools.copy_state_dict(model, stable_audio_tools.load_ckpt_state_dict(path_model_ckpt_path))
+#     strategy = 'ddp_find_unused_parameters_true' if num_gpus > 1 else "auto" 
 
-# sample_rate = model_config["sample_rate"]
-# sample_size = model_config["sample_size"]
-
-# model = model.to(device)
-
-# conditioning = [{
-#     "prompt": "128 BPM tech house drum loop",
-#     "seconds_start": 0, 
-#     "seconds_total": 180
-# }]
-
-# # Generate stereo audio
-# output = stable_audio_tools.generate_diffusion_cond(
-#     model,
-#     steps=100,
-#     cfg_scale=7,
-#     conditioning=conditioning,
-#     sample_size=sample_size,
-#     sigma_min=0.3,
-#     sigma_max=500,
-#     sampler_type="dpmpp-3m-sde",
-#     device=device
-# )
-
-''''''
-
-
-# args = [
-#     "python3", "stable-audio-tools/train.py",
-#     "--dataset-config", "dataset.json",
-#     "--model-config", "model_config.json",
-#     "--name", "stable_audio_open_finetune",
-#     "--save-dir", "checkpoints",
-#     "--checkpoint-every", "1000",
-#     "--batch-size", "32",
-#     "--num-gpus", "2",
-#     "--precision", "16-mixed",
-#     "--seed", "128",
-#     "--pretrained-ckpt-path", "model.ckpt",
-# ]
-
-# subprocess.run(args=args)
-# python3 stable-audio-tools/train.py --dataset-config dataset.json --model-config model_config.json --name test
+#     trainer = pl.Trainer(
+#             devices=num_gpus,
+#             accelerator="gpu",
+#             num_nodes = 1,
+#             strategy=strategy,
+#             precision="16-mixed",
+#             accumulate_grad_batches=1, 
+#             callbacks=[ckpt_callback, demo_callback, exc_callback, save_model_config_callback],
+#             logger=wandb_logger,
+#             log_every_n_steps=1,
+#             max_epochs=10000000,
+#             default_root_dir=path_save_dir,
+#             gradient_clip_val=0.0,
+#             reload_dataloaders_every_n_epochs = 0
+#         )
+#     trainer.fit(training_wrapper, train_dl, ckpt_path=ckpt_path if ckpt_path else None)
---
+++ util.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+from pathlib import Path
+import subprocess as sp
+
+def unpack(ziptar: str, dest: str):
+    dext = os.path.splitext(ziptar)[1]
+    if dext == ".zip":
+        sp.call(["unzip", "-j", str(ziptar), "-d", f"{dest}/"])
+    elif dext in [".tar", ".gz", ".tgz", ".tar.gz"]:
+        sp.call(["tar", "-xvaf", str(ziptar), "-C", f"{dest}/", "--strip-components=1"])
+    else:
+        raise Exception(
+            "Not supported compression file type. The file type should be one of 'zip', 'tar', 'tar.gz', 'tgz' types of compression file."
+        )
+
+    # Removing junk files
+    for item in ["__MACOSX", ".DS_Store"]:
+        item_path = Path(f"{dest}/{item}")
+        if item_path.exists():
+            shutil.rmtree(str(item_path), ignore_errors=True)
